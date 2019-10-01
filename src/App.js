4400d3eb096525c51ada16b3bdd441d94c29b334
sergiovelo17 <sergiovelo17@gmail.com>
2019-10-01 11:04:15 -0400

    testing components and switching routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Nav from './Components/navbar/navbar'
+import TestPage from './Components/testpage/testpage'
 import {Route, Link, Switch} from 'react-router-dom';
 class App extends Component {
   render(){
@@ -8,6 +9,11 @@ class App extends Component {
     <div className="App">
      <Nav/>
      <img src='/images/My Post.jpg'></img>
+     <Switch>
+     <Route exact path="/this" render ={(props)=> <TestPage
+           {...props} 
+           />} />
+    </Switch>
     </div>
   );
 }
---
+++ src/Components/testpage/testpage.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Route, Link, Switch} from 'react-router-dom';
+class Test extends Component {
+    render(){
+    return (
+      <div>
+       <h1>HEY</h1>
+      </div>
+    );
+  }
+  }
+export default Test
